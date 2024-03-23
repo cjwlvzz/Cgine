48b3bb92cabbe3a02d56a7185a3ef4e69f9db16f
cjwlvzz <976136404@qq.com>
2024-03-23 13:28:18 +0800

    Add project files.

---
+++ CgineEditor/GameProject/ProjectBrowserDialg.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CgineEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialg.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialg : Window
+    {
+        public ProjectBrowserDialg()
+        {
+            InitializeComponent();
+        }
+    }
+}
