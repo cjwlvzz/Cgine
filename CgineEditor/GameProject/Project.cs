cfc2e84e932f145a1679084955c8d4dc9c4bea1a
cjwlvzz <976136404@qq.com>
2024-03-26 00:02:49 +0800

    adding scene class

---
+++ CgineEditor/GameProject/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CgineEditor.GameProject
+{
+    class Project : ViewModelBase
+    {
+        public string Name { get; private set ; }
+
+        public string Path { get; private set; }
+
+        public static string Extension { get; } = ".cgine";
+
+        public string FullPath => $"{Path}{Name}{Extension}";
+
+        private ObservableCollection<Scene> _scenes = new ObservableCollection<Scene>();
+
+        //The Collection for the xml to read
+        public ReadOnlyObservableCollection<Scene> Scenes { get; }
+
+    }
+
+   
+
+}
---
+++ CgineEditor/GameProject/Scene.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CgineEditor.GameProject
+{
+    class Scene :ViewModelBase
+    {
+    }
+}
