fcb838b50122f686679adcda2ee8b41fb4f4f419
cjwlvzz <976136404@qq.com>
2024-03-26 00:07:24 +0800

    scene class

--- CgineEditor/GameProject/Scene.cs
+++ CgineEditor/GameProject/Scene.cs
@@ -6,5 +6,18 @@ namespace CgineEditor.GameProject
 {
     class Scene :ViewModelBase
     {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(Name);
+                }
+            }
+        }
     }
 }
