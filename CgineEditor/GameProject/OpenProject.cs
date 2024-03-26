2480699e9b4a6d6df33c2ed7608a6f6210a54583
cjwlvzz <976136404@qq.com>
2024-03-26 21:21:50 +0800

    open project file

---
+++ CgineEditor/GameProject/OpenProject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CgineEditor.GameProject
+{
+
+    [DataContract]
+    public class ProjectData
+    {
+        [DataMember]
+        public string ProjectName { get; set; }
+
+        [DataMember]
+        public string ProjectPath { get; set; }
+
+        [DataMember]
+        public DateTime Data { get; set; }
+
+    }
+
+    [DataContract]
+    public class ProjectDataList
+    {
+        [DataMember]
+        public List<ProjectData> Projects { get; set; }
+    }
+
+    //to remember where project and sava that data into the file
+    class OpenProject
+    {
+        private static readonly string _applicationDataPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\CgineEditor\";
+        private static readonly string _projecDataPath;
+        static OpenProject()
+        {
+
+        }
+    }
+}
