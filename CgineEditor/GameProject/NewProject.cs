3fae3b17279012ce7fe4d170e171af2dadc624fc
cjwlvzz <976136404@qq.com>
2024-04-11 18:09:37 +0800

    fix the bug that project xml can be generated properly

--- CgineEditor/GameProject/NewProject.cs
+++ CgineEditor/GameProject/NewProject.cs
@@ -186,7 +186,7 @@ namespace CgineEditor.GameProject
                 var projectPath = Path.GetFullPath(Path.Combine(path, $"{ProjectName}{Project.Extension}"));
                 File.WriteAllText(projectPath, projectXmlConfig);
 
-                var project = new Project(ProjectName, path);
+                var project = Serializer.FromFile<Project>(projectPath);
                 Serializer.ToFile(project, path + $"{ProjectName}" + Project.Extension);
                 return path;
 
