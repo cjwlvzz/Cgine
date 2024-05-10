2ac91a6d1cb9bdcbde708375cd5b7fffbc3dea13
cjwlvzz <976136404@qq.com>
2024-05-10 19:16:35 +0800

    adding c++ headers

---
+++ Cgine/Common/CommonHeaders.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdint.h>
+#include <assert.h>
+
+// common headers
+#include "PrimitiveTypes.h"
---
+++ Cgine/Common/PrimitiveTypes.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdint.h>
+//unsigned intergers,unreal like
+using uint8 = uint8_t;
+using uint16 = uint16_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
+//signed intergers,unreal like
+using int8 = int8_t;
+using int16 = int16_t;
+using int32 = int32_t;
+using int64 = int64_t;
+
+//Invalid identifier
+constexpr uint64 uint64_invalid_id{ 0xffff'ffff'ffff'ffffui64 };
+constexpr uint32 uint32_invalid_id{ 0xffff'ffffui32 };
+constexpr uint16 uint16_invalid_id{ 0xffffui16 };
+constexpr uint8 uint8_invalid_id{ 0xffui8 };
